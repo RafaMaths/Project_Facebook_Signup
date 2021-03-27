43b1be1a8dad41cc6266d4066e2f2331d0840621
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-03-27 11:57:23 -0300

    Adiciona o desafio 20

--- script.js
+++ script.js
@@ -34,28 +34,58 @@ function checkInputValues() {
   return emptyInput;
 }
 
-// function customizeRightContent () {
-//   const rightContent = document.querySelector('.right-content');
-//   rightContent.innerHTML = '';
-//   const customH3 = document.createElement('h3');
+const takesSelectedGender = () => {
+  const gender = document.getElementsByName('gender');
+  let selectedGender;
+  for (let i = 0; i < gender.length; i += 1) {
+    if (gender[i].checked) {
+      selectedGender = gender[i];
+    }
+  }
+  return selectedGender;
+};
 
-//   customH3.innerText = 'teste';
-//   rightContent.appendChild(customH3);
-// }
+const createInputObj = () => {
+  const inputObj = {};
+  const firstName = document.getElementById('firstname');
+  const lastName = document.getElementById('lastname');
+  const phoneEmail = document.getElementById('phone_email');
+  console.log(phoneEmail);
+  const birthDate = document.getElementById('birth-date');
+  const selectedGender = takesSelectedGender();
+  inputObj.firstName = firstName.value;
+  inputObj.lastName = lastName.value;
+  inputObj.phoneEmail = phoneEmail.value;
+  inputObj.birthDate = birthDate.value;
+  console.log(inputObj);
+  inputObj.selectedGender = selectedGender.value;
+  return inputObj;
+};
+
+function customizeRightContent() {
+  const rightContent = document.querySelector('.right-content');
+  const inputObj = createInputObj();
+  rightContent.innerHTML = '';
+  const customH3 = document.createElement('h3');
+  customH3.innerText = `Olá, ${inputObj.firstName} ${inputObj.lastName}`;
+  rightContent.appendChild(customH3);
+  const customP = document.createElement('p');
+  customP.innerText = `Celular/E-mail: ${inputObj.phoneEmail}
+  Data de Nascimento: ${inputObj.birthDate}
+  Gênero: ${inputObj.selectedGender}`;
+  rightContent.appendChild(customP);
+}
 
 const submitBtn = document.getElementById('facebook-register');
 submitBtn.addEventListener('click', () => {
   if (checkInputValues() > 2) {
     const alert = document.getElementById('alert');
     alert.innerText = 'Campos inválidos';
+  } else {
+    customizeRightContent();
   }
-  // else {
-  //   customizeRightContent();
-  // }
 });
 
-function returnFalse() {
-  return false;
-}
+const returnFalse = () => false;
 
 returnFalse();
